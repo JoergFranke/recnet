bugfix in cross entropy loss function, softmax and regularization

recnet/build_model.py
@@ -44,8 +44,7 @@ class rnnModel(ModelMaster):
         ########################################
         self.X_tv2 = T.tensor3('X_tv2', dtype=theano.config.floatX)
         if self.prm.optimize["CTC"]:
-            #self.Y_tv2 = T.ivector('Y_tv2', )
-            self.Y_tv2 = T.imatrix('Y_tv2', ) #todo rebuild 2, batch size 1, no batch dimension
+            self.Y_tv2 = T.imatrix('Y_tv2')
         else:
             self.Y_tv2 = T.tensor3('Y_tv2', dtype=theano.config.floatX)
         self.M_tv2 = T.tensor3('M_tv2', dtype=theano.config.floatX)
@@ -54,7 +53,7 @@ class rnnModel(ModelMaster):
 
         ######           Create model parameters
         ########################################
-        tpo = OrderedDict() #theano optimization parameter
+        tpo = OrderedDict()
         for key, value in self.prm.optimize.items():
             if not isinstance(value,str):
                 if not isinstance(value, int):
@@ -77,7 +76,7 @@ class rnnModel(ModelMaster):
         for i in np.arange(1, self.prm.struct["hidden_layer"]+2):
             network_layer.append(SuperLayer(self.rng,self.trng, self.prm.struct,self.prm.data, i, old_weights[i-1]))
 
-        self.layer_weights = [l.weights for l in network_layer] # + [output_layer.weights]
+        self.layer_weights = [l.weights for l in network_layer]
         self.all_weights = sum([l for l in self.layer_weights],[])
 
 
@@ -88,7 +87,7 @@ class rnnModel(ModelMaster):
         except AttributeError:
             raise NotImplementedError("Class `{}` does not implement `{}`".format(loss_function.__name__, self.prm.optimize["loss_function"]))
 
-        loss_fnc = loss_function_(tpo, self.prm.data["batch_size"]) #w2_cross_entropy() #cross_entropy() #
+        loss_fnc = loss_function_(tpo, self.prm.data["batch_size"])
 
 
         ######                    Connect layers
@@ -119,10 +118,10 @@ class rnnModel(ModelMaster):
         def regularization(weights):
             w_error = 0
             for w in weights:
-                w_error = w_error + tpo["reg_factor"]  * T.square(T.mean(T.sqr(w)))
+                w_error = w_error + tpo["reg_factor"] * T.square(T.mean(T.sqr(w)))
             return w_error
 
-        if tpo["regularization"]:
+        if self.prm.optimize["regularization"] == True:
             self.t_error = o_error + regularization(self.all_weights)
         else:
             self.t_error = o_error
@@ -139,7 +138,7 @@ class rnnModel(ModelMaster):
 
         self.updates = optimizer.fit(self.all_weights,self.t_error, tpo)
 
-        #self.gradients = optimizer.grads todo remove
+
     ######            BUILD THEANO FUNCTIONS
     ########################################
     def get_training_function(self):

recnet/layer_pool/output_layer.py
@@ -21,12 +21,18 @@ class softmax(LayerMaster):
         self.n_in = n_in
         self.n_out = n_out
 
-        #output layer
-        w_out_np2 = self.rec_uniform_sqrt(rng, self.n_in, self.n_out)
-        b_out_np2 = np.zeros(self.n_out)
+        #w_out_np2 = self.rec_uniform_sqrt(rng, self.n_in, self.n_out)
 
-        w_out_np2 = 1 * (np.random.rand(self.n_in, self.n_out) - 0.5) #self.rec_uniform_sqrt(rng, self.n_in, self.n_out)
-        b_out_np2 = 1 * (np.random.rand(self.n_out) - 0.5) #np.zeros(self.n_out)
+        #w_out_np2 = 1 * (rng.rand(self.n_in, self.n_out) - 0.5)
+        #b_out_np2 = 1 * (rng.rand(self.n_out) - 0.5)
+
+        w_out_np2 = rng.uniform(-np.sqrt(1./self.n_in), np.sqrt(1./self.n_in), (self.n_in, self.n_out))
+        b_out_np2 = rng.uniform(-np.sqrt(1./self.n_in), np.sqrt(1./self.n_in), self.n_out)
+
+        #w_out_np2 = 0.01 * rng.randn(self.n_in, self.n_out)
+        #b_out_np2 = np.ones(self.n_out)
+
+        # todo initialization
 
         if old_weights == None:
             self.t_w_out = theano.shared(name='w_out', value=w_out_np2.astype(T.config.floatX))
@@ -42,65 +48,24 @@ class softmax(LayerMaster):
         self.weights = [self.t_w_out,self.t_b_out]
 
 
-    # def vanilla(self, output):
-    #     net_o = T.add( T.dot(output , self.t_w_out) , self.t_b_out )
-    #     result, updates = theano.map(T.nnet.softmax, net_o)
-    #     return result
-
-    def _drop_out_softmax(self, signal, mask,t_w_out,t_b_out, use_dropout, dropout_value):
-        d_w_out = T.switch(use_dropout,
-                             (t_w_out *
-                              self.trng.binomial(t_w_out.shape,
-                                            p=dropout_value, n=1,
-                                            dtype=t_w_out.dtype)),
-                             t_w_out)
-
-        net_o = T.add( T.dot(signal , d_w_out) , t_b_out)
-        output = T.nnet.softmax(net_o)
-
-        mask = T.addbroadcast(mask, 1) #todo rechange
-        output = mask * output   + (1. - mask) * 0.1**8
-
-        return output
-
-    # def sequence_iteration(self, output, mask,use_dropout=0,dropout_value=0.5):
-    #     prm = [self.t_w_out, self.t_b_out, use_dropout,dropout_value]
-    #     result, updates = theano.map(self._drop_out_softmax, [output, mask], prm)
-    #     return result #todo rechange
-
     def sequence_iteration(self, output, mask,use_dropout=0,dropout_value=0.5):
 
         dot_product = T.dot(output , self.t_w_out)
 
         net_o = T.add( dot_product , self.t_b_out )
 
-        #mask = mask[:,0,:]
-        #output = output[:,0,:]
-
-        #net_o = T.add( T.dot(output , self.t_w_out) , self.t_b_out)
-
-
         ex_net = T.exp(net_o)
-        #e_x = np.exp(x - np.max(x))
-
         sum_net = T.sum(ex_net, axis=2, keepdims=True)
-
         softmax_o = ex_net / sum_net
 
 
-        mask = T.addbroadcast(mask, 2) #todo rechange
-        #output = mask * output   + (1. - mask) * 0.1**8
-        output = T.mul(mask, softmax_o)   + T.mul( (1. - mask) , 0.1**8 )
-
-        #result = self._drop_out_softmax(output[:,0,:], mask[:,0,:],self.t_w_out, self.t_b_out, use_dropout , 1)
-
-        #prm = [self.t_w_out, self.t_b_out, use_dropout,dropout_value]
-        #result, updates = theano.map(self._drop_out_softmax, [output, mask], prm)
+        mask = T.addbroadcast(mask, 2) # todo nesseccary?
+        output = T.mul(mask, softmax_o)   + T.mul( (1. - mask) , 1e-6 )
 
         return output #result
 
 
-### TEST FUNCTIONS
+### TEST FUNCTIONS # todo make new file with test functions
 from scipy.stats import multivariate_normal
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 

recnet/layer_pool/recurrent_layer.py
@@ -55,7 +55,7 @@ class conv(LayerMaster):
             for pp in old_weights:
                 self.weights.append(theano.shared(value=pp.astype(T.config.floatX)))
 
-        # Init last output and cell state #todo init rausziehn
+        # Init last output and cell state #todo make initialization of hidden state lernable
         #init_hidden = 1 * (np.random.rand(n_batches, n_out) - 0.5)
         #init_hidden = init_hidden.astype(dtype=theano.config.floatX)
         init_hidden = np.zeros([n_batches, n_out]).astype(dtype=theano.config.floatX)
@@ -125,16 +125,29 @@ class LSTMp(LayerMaster):
 
             np_weights = OrderedDict()
             # Peephole weights (input- forget- output- gate)
-            np_weights['w_ig_c'] = self.vec_uniform_sqrt(self.rng, n_out)
-            np_weights['w_fg_c'] = self.vec_uniform_sqrt(self.rng,
-                                                             n_out) + 2  # Forgot gate with +2 initialized for keeping sequences right from begin
-            np_weights['w_og_c'] = self.vec_uniform_sqrt(self.rng, n_out)
+            # np_weights['w_ig_c'] = self.vec_uniform_sqrt(self.rng, n_out)
+            # np_weights['w_fg_c'] = self.vec_uniform_sqrt(self.rng, n_out) + 2  # Forgot gate with +2 initialized for keeping sequences right from begin
+            # np_weights['w_og_c'] = self.vec_uniform_sqrt(self.rng, n_out)
+            # # Previous output weights
+            # np_weights['w_ifco'] = self.rec_ortho(rng, n_out, 4)
+            # np_weights['b_ifco'] = np.zeros(4 * n_out)
+            # # Input weights
+            # np_weights['w_ifco_x'] = self.rec_uniform_sqrt(rng, n_in, 4 * n_out)
+            # np_weights['b_ifco_x'] = np.zeros(4 * n_out)
+
+
+            # todo initialization
+            np_weights['w_ig_c'] = rng.uniform(-np.sqrt(1./n_out), np.sqrt(1./n_out), n_out)
+            np_weights['w_fg_c'] = rng.uniform(-np.sqrt(1./n_out), np.sqrt(1./n_out), n_out)+2 #Forgot gate with +2 initialized for keeping sequences right from begin
+            np_weights['w_og_c'] = rng.uniform(-np.sqrt(1./n_out), np.sqrt(1./n_out), n_out)
+
             # Previous output weights
-            np_weights['w_ifco'] = self.rec_ortho(rng, n_out, 4)
-            np_weights['b_ifco'] = np.zeros(4 * n_out)
+            np_weights['w_ifco'] = rng.uniform(-np.sqrt(1./n_out), np.sqrt(1./n_out), (n_out, 4*n_out))
+            np_weights['b_ifco'] = np.zeros(4*n_out)
             # Input weights
-            np_weights['w_ifco_x'] = self.rec_uniform_sqrt(rng, n_in, 4 * n_out)
-            np_weights['b_ifco_x'] = np.zeros(4 * n_out)
+            np_weights['w_ifco_x'] = rng.uniform(-np.sqrt(1./n_out), np.sqrt(1./n_out), (n_in, 4*n_out))
+            np_weights['b_ifco_x'] = np.zeros(4*n_out)
+
 
             self.weights = []
             for kk, pp in np_weights.items():